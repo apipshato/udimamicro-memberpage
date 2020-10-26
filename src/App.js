923251fcfde391a4a83b390c140650ca77464cce
apipshato <afifshatos@gmail.com>
2020-10-26 15:30:38 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './assets/css/style.css';
+import React  from 'react';
+
+
+function App() {
+  return (
+    <div className="container mx-auto ">
+      <h1 className="text-6xl">javascript react</h1>
+    </div>
+  );
+}
+
+export default App;
